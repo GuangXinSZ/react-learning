d1a3d2cc5d2dcd80daa4af467825b443c90feb70
xin <1446854524@qq.com>
2018-05-04 21:45:09 +0800

    Redux
    简单时间react——redux

---
+++ React_redux/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import Counter from './components/Counters';
+import counter from './reducers';
+
+const store = createStore(counter);
+
+const render = () =>ReactDOM.render(
+	<Counter
+	value={store.getState()}
+	onIncrement={()=>store.dispatch({ type: 'INCREMENT' })}
+	onDecrement={()=>store.dispatch({ type: 'DECREMENT' })}
+	/>,
+	document.getElementById('root')
+);
+render()
+store.subscribe(render);
+ 
