ff0651bad0f09566f20da6fc8ce63172ff48b9ed
liugx <18379177354@163.com>
2018-11-14 12:57:23 +0800

    feature: redux。

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from './store/mini-redux';
 import { counter } from './store/index';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(counter);
 
-function render() {
-  ReactDOM.render(<App store={store} />, document.getElementById('root'));
-}
-
-render();
-// 每次修改状态,从新渲染页面
-store.subscribe(render);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+   document.getElementById('root')
+)
 
 serviceWorker.unregister();
