ba7ed4ac93f7e1bd72003fff76b90b09990d3043
liugx <18379177354@163.com>
2019-03-29 11:16:21 +0800

    feature: 全屏。

--- src/view/header/Index.jsx
+++ src/view/header/Index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon } from 'antd'
+import { Icon, Tooltip } from 'antd'
 
 import '../../assets/index/header.scss'
 
@@ -8,8 +8,41 @@ export default class Index extends React.Component {
     super()
 
     this.state = {
-
+      title: '全屏',
+      fullscreen: false
     }
+  }
+
+  // 全屏点击触发事件
+  handleFullScreen = () => {
+    let { fullscreen } = this.state
+    let element = document.documentElement
+
+    if (fullscreen) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen()
+      } else if (document.webkitCancelFullScreen) {
+        document.webkitCancelFullScreen()
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen()
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen()
+      }
+    } else {
+      if (element.requestFullscreen) {
+        element.requestFullscreen()
+      } else if (element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen()
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen()
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen()
+      }
+    }
+
+    this.setState({
+      fullscreen: !fullscreen
+    })
   }
 
   componentDidMount () {
@@ -21,10 +54,17 @@ export default class Index extends React.Component {
       <div className="head">
         <div className="left">
           <Icon type="windows"  style={style.iconColor}/>
-          后台管理系统
+          商户管理系统
         </div>
-        <div className="right" >
-          全屏
+        <div className="right">
+          <div style={style.flexSetting}>
+            <Tooltip placement="topLeft" title={this.state.title} overlayStyle={style.cart} onClick={this.handleFullScreen}>
+              <Icon type="fullscreen" style={style.iconColor}/>
+            </Tooltip>
+            <div>
+              2
+            </div>
+          </div>
         </div>
       </div>
     ) 
@@ -32,6 +72,8 @@ export default class Index extends React.Component {
 }
 
 const style = {
-  bgColor: {background: '#001529', color: 'white'},
-  iconColor: { color: '#1890ff', marginRight: '10px' }
+  bgColor: { background: '#001529', color: 'white' },
+  iconColor: { color: '#1890ff', marginRight: '12px', fontSize: '20px' },
+  flexSetting: { display: 'flex' },
+  cart: { color: 'red' }
 }
