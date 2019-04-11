686c317eaea54892b7fa29c883189a65ea806d86
liugx <18379177354@163.com>
2019-04-11 18:59:17 +0800

    feature: 添加首页表单。

--- src/view/Statistical/index.jsx
+++ src/view/Statistical/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import Page from '../../component/Pagination'
+import { Table } from 'antd'
 
 class Movie extends React.Component{
   constructor(props) {
     super(props)
 
     this.state = {
-      totail: 50
+      totail: 50,
+      saleList: [],
+      columns: [
+        {
+          title: '日期',
+          dataIndex: 'name'
+        },{
+          title: '下单笔数',
+          className: 'column-money',
+          dataIndex: 'money',
+        },{
+          title: '付款订单',
+          dataIndex: 'address',
+        },{
+          title: '类型订单',
+          dataIndex: 'type',
+        }
+      ]
     }
   }
 
@@ -19,11 +37,22 @@ class Movie extends React.Component{
 
   render () {
     return (
-      <div>
+      <div style={style.content}>
+        <div>
+          <Table
+            columns={this.state.columns}
+            dataSource={this.state.saleList}
+            bordered
+          />,
+        </div>
         <Page fetchData={this.fetchData} totail={this.state.totail}></Page>
       </div>
     )
   }
+}
+
+const style = {
+  content: {padding: '20rpx'}
 }
 
 export default Movie
